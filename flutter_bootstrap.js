6d250ffd200c1ff98db9fba3855ce03f8b4e6201
Ammar Ahmad <hammadmalli64@gmail.com>
2024-12-02 21:00:13 +0500

    2 Dec

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"db49896cf25ceabc44096d5f088d86414e05a7
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1783486404"
+    serviceWorkerVersion: "2284848291"
   }
 });
