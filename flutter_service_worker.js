b4f432d7183a7d9c47c948e37e677bd2d02e32f4
UsmanAsad87 <usmanasad0324@gmail.com>
2025-01-16 01:13:39 +0500

    code pushed

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,12 +3,12 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "1d1d404fe05edbf1ee520cf83b5cc7a0",
+const RESOURCES = {"flutter_bootstrap.js": "a40b0887f1be8ba4493b3d2b074b347a",
 "version.json": "669e61596666b5b7bba64d87d1ccb278",
 "favicon.ico": "64a301b3dc351769e3bc9954be59f942",
 "index.html": "6bf0119f381e977ac5b4b1012a1f29e5",
 "/": "6bf0119f381e977ac5b4b1012a1f29e5",
-"main.dart.js": "881bb530f54899ab08b4223b15b12aea",
+"main.dart.js": "ebf4c547be9e87b797b9c001ee743f8f",
 "flutter.js": "f393d3c16b631f36852323de8e583132",
 "icons/Icon-192.png": "5afb439c4b6466bb1ae5b354af5d10bb",
 "icons/Icon-maskable-192.png": "5afb439c4b6466bb1ae5b354af5d10bb",
