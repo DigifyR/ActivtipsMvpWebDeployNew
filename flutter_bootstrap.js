09e20c6c9a79d3bc48313c91c6964ac704d2d681
uzair429 <uzairakhunzada@gmail.com.com>
2025-10-27 19:55:47 +0500

    27 oct

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"db49896cf25ceabc44096d5f088d86414e05a7
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3989678313"
+    serviceWorkerVersion: "1078992907"
   }
 });
