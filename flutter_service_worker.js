c341069950772c9ebac59b0aa64ca1611351072e
uzair429 <uzairakhunzada@gmail.com.com>
2025-12-24 23:10:37 +0500

    24 dec 2025

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,12 +3,12 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "82bd9e1f03563dccab7c405fb15fdf69",
+const RESOURCES = {"flutter_bootstrap.js": "1396de19eaa7875f812bc710014bb94c",
 "version.json": "669e61596666b5b7bba64d87d1ccb278",
 "favicon.ico": "e9c7fdcefee3424f843eb216bdd4b1b7",
 "index.html": "fe4c06c20753447ec3f2e1dcaedabdd9",
 "/": "fe4c06c20753447ec3f2e1dcaedabdd9",
-"main.dart.js": "da39a5b82a9370c0480ac2a54402a7ed",
+"main.dart.js": "601807cf4b26bfec934325ccdb9ce8e4",
 "flutter.js": "f393d3c16b631f36852323de8e583132",
 "icons/Icon-192.png": "c6c891486c5f379f8e5df44f80846dfe",
 "icons/Icon-maskable-192.png": "c6c891486c5f379f8e5df44f80846dfe",
